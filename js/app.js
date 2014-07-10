7da88de8f94ae99c117fa25db0f8dd15aa33591f
Antony Grove <agrove86@gmail.com>
2014-07-09 22:57:37 -0500

    1st round
    add list item, delete list item functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
+	$('#main-submit').click(function(event){
+		addToList();
+		event.preventDefault();  // prevents the page from reloading
+	});  // end click main submit button
+
+	$('.add-list').on('click', '#delete-icon', function(){
+		$(this).parent().remove();
+	});
+
 
 }); // Ready handler end
+
+
+function addToList() {
+	var value = $('#main-input').val();   // get value of input field
+
+		$('.add-list').append('<li class="clearfix">' + 
+			value + '<button id="delete-icon">' + 
+			'</button>' + '<button class="edit-icon">' + 
+			'</button>' + '</li>');
+				
+	    $('#main-input').val(''); // reset text field
+}
