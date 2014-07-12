1b2f45008a8dfcf2b5dad3a3f83abfe1e0e696f6
Antony Grove <agrove86@gmail.com>
2014-07-11 21:49:38 -0500

    edit done
    added edit funtionality

--- js/app.js
+++ js/app.js
@@ -9,6 +9,17 @@ $(document).ready(function(){
 		$(this).parent().remove();
 	});
 
+	$('.add-list').on('click', '#edit-icon', function(){
+		$(this).closest('li').hide().next().show().css('display', 'block').val($(this).prev().prev().text());
+	});
+
+	$('.add-list').on('focusout', '#edit', function(){
+		$(this).hide().prev().show().children(":first").text($(this).val());
+		
+	});
+
+	
+ 
 	
 
 
@@ -18,10 +29,12 @@ $(document).ready(function(){
 function addToList() {
 	var value = $('#main-input').val();   // get value of input field
 
-		$('.add-list').append('<li class="clearfix">' + 
-			value + '<button id="delete-icon">' + 
-			'</button>' + '<button class="edit-icon">' + 
-			'</button>' + '</li>');
+		$('.add-list').append('<li class="clearfix">' + '<span>' + value + '</span>'
+			 + '<button id="delete-icon">' + 
+			'</button>' + '<button id="edit-icon">' + 
+			'</button>' + '</li>' + '<input type="text" id="edit">');
 				
 	    $('#main-input').val(''); // reset text field
-}
+};
+
+
